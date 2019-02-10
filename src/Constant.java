449db32d1e9d74b95e45cf17f6446c39344d5526
Maggie Lehman <ejbmj7@gmail.com>
2019-02-10 13:28:20 -0500

    Completed the function abstract class including methods and added the constant, sum, and variable classes which extend the function class

---
+++ src/Constant.java
@@ -0,0 +1,26 @@
+public class Constant extends function {
+    @Override
+    public double evaulate() {
+        return 0;
+    }
+
+    @Override
+    public function derivative() {
+        return null;
+    }
+
+    @Override
+    public double integral() {
+        return 0;
+    }
+
+    @Override
+    public boolean isConstant() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
